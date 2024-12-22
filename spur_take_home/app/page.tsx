4e55aa2c02a9e243f29ef9a189eaede44e45ec88
Jayanth Peetla <ynv7jk@virginia.edu>
2024-12-22 14:28:36 -0500

    created custom modal with test info.

--- spur_take_home/app/page.tsx
+++ spur_take_home/app/page.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/Modal";
 import { CustomCalendar } from "@/components/Calendar";
-import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export default function Page() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-start pt-10 pl-10">
       <h1 className="text-2xl font-bold mb-5">Scheduled Suites</h1>
-      <Button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-2">
+      <Button
+        onClick={() => setIsModalOpen(true)}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-2"
+      >
         + Schedule Test
       </Button>
-
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       <div className="w-full h-[90vh] px-10 pt-5">
         <CustomCalendar />
       </div>
