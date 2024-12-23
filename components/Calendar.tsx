48d50cb251e3ce276865c87ad2e2bacd1cb75134
Jayanth Peetla <ynv7jk@virginia.edu>
2024-12-23 10:08:48 -0500

    fixed UTC to EST time conversion error

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { fetchSchedule } from "@/hooks/useSchedule";
+import { fetchSchedule } from "@/utils/fetchSchedule";
 
 const localizer = momentLocalizer(moment);
 
